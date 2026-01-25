ca35b350264e11a2b2684ed9d8e23c29e8c31529
shifang <291596789@qq.com>
2026-01-25 15:32:11 +0800

    SEO: redirect root to /en with 308

--- proxy.ts
+++ proxy.ts
@@ -29,6 +29,12 @@ export default function middleware(request: NextRequest) {
     }
   }
 
+  if (request.nextUrl.pathname === "/") {
+    const url = request.nextUrl.clone();
+    url.pathname = "/en";
+    return NextResponse.redirect(url, 308);
+  }
+
   return intlMiddleware(request);
 }
 
