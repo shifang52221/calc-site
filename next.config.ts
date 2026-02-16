6627b426573c257f4287c5faf89c79b6869d4f79
shifang <291596789@qq.com>
2026-02-16 08:35:45 +0800

    Noindex social preview image endpoints

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,28 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async headers() {
+    const noindexHeader = { key: "X-Robots-Tag", value: "noindex" };
+    return [
+      {
+        source: "/opengraph-image",
+        headers: [noindexHeader],
+      },
+      {
+        source: "/twitter-image",
+        headers: [noindexHeader],
+      },
+      {
+        source: "/:locale/opengraph-image",
+        headers: [noindexHeader],
+      },
+      {
+        source: "/:locale/twitter-image",
+        headers: [noindexHeader],
+      },
+    ];
+  },
+};
 
 export default withNextIntl(nextConfig);
